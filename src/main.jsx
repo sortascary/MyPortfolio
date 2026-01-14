e85f0a1aee49e5ef280ff6c4e209547b6051603a
sortascary <66732198+sortascary@users.noreply.github.com>
2026-01-14 10:32:32 +0000

    fix: github pages fix attempt again

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
